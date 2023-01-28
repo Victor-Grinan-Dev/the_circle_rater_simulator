31d44e33d066402e74cfbefb845b689282cf1f7d
Victor <Victor.grinan@free.fi>
2023-01-28 09:50:56 +0200

    create classes player and rates

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+import { Player } from './classes/circlePlayer';
+import { RatingRound } from './classes/ratingRound';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>The Circle</h1>
+      </header>
+      <div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
